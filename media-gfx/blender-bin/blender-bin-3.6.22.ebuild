negril/gentoo

commit 6a3644bf2f9aace46ad9bbab2e4f3bccd747079e

*/*: Add := to virtual/zlib deps, part 1
Done via:

```
git grep -l virtual/zlib$ | xargs sed -i -e 's@virtual/zlib$@&:=@'
```

Signed-off-by: Michał Górny <mgorny@gentoo.org>

media-gfx/blender-bin/blender-bin-3.6.22.ebuild
@@ -38,7 +38,7 @@ RDEPEND="
 	media-libs/libglvnd[X]
 	sys-libs/glibc
 	sys-libs/ncurses
-	virtual/zlib
+	virtual/zlib:=
 	virtual/libcrypt
 	x11-base/xorg-server
 	x11-libs/libICE

media-gfx/blender-bin/blender-bin-4.2.9.ebuild
@@ -39,7 +39,7 @@ RDEPEND="
 	sys-apps/util-linux
 	sys-libs/glibc
 	sys-libs/ncurses
-	virtual/zlib
+	virtual/zlib:=
 	virtual/libcrypt
 	x11-base/xorg-server
 	x11-libs/libICE

media-gfx/blender-bin/blender-bin-4.4.3.ebuild
@@ -39,7 +39,7 @@ RDEPEND="
 	sys-apps/util-linux
 	sys-libs/glibc
 	sys-libs/ncurses
-	virtual/zlib
+	virtual/zlib:=
 	virtual/libcrypt
 	x11-base/xorg-server
 	x11-libs/libICE

commit 6035df2df4b739b7f3b2b4df3a688fdbcad59c06

media-gfx/blender-bin: depend on actual zlib slot as this is prebuild
Fixes: 6a3644bf2f9aace46ad9bbab2e4f3bccd747079e
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

media-gfx/blender-bin/blender-bin-3.6.22.ebuild
@@ -38,7 +38,7 @@ RDEPEND="
 	media-libs/libglvnd[X]
 	sys-libs/glibc
 	sys-libs/ncurses
-	virtual/zlib:=
+	virtual/zlib:0/1
 	virtual/libcrypt
 	x11-base/xorg-server
 	x11-libs/libICE

media-gfx/blender-bin/blender-bin-4.2.9.ebuild
@@ -39,7 +39,7 @@ RDEPEND="
 	sys-apps/util-linux
 	sys-libs/glibc
 	sys-libs/ncurses
-	virtual/zlib:=
+	virtual/zlib:0/1
 	virtual/libcrypt
 	x11-base/xorg-server
 	x11-libs/libICE

media-gfx/blender-bin/blender-bin-4.4.3.ebuild
@@ -39,8 +39,8 @@ RDEPEND="
 	sys-apps/util-linux
 	sys-libs/glibc
 	sys-libs/ncurses
-	virtual/zlib:=
 	virtual/libcrypt
+	virtual/zlib:0/1
 	x11-base/xorg-server
 	x11-libs/libICE
 	x11-libs/libSM