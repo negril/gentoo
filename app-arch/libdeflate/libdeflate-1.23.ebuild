negril/gentoo

commit d4a25b403dab1c6a6cf69492d27a433519a5619e

*/*: Add := to virtual/zlib deps, part 4
Done via:

```
git grep -l 'virtual/zlib\[' |
  xargs sed -i -e 's@virtual/zlib\[@virtual/zlib:=\[@g'
```

Signed-off-by: Michał Górny <mgorny@gentoo.org>

app-arch/libdeflate/libdeflate-1.23.ebuild
@@ -27,7 +27,7 @@ RESTRICT="
 "
 
 DEPEND="
-	test? ( virtual/zlib[${MULTILIB_USEDEP}] )
+	test? ( virtual/zlib:=[${MULTILIB_USEDEP}] )
 "
 
 PATCHES=(

app-arch/libdeflate/libdeflate-1.24.ebuild
@@ -31,7 +31,7 @@ RESTRICT="
 "
 
 DEPEND="
-	test? ( virtual/zlib[${MULTILIB_USEDEP}] )
+	test? ( virtual/zlib:=[${MULTILIB_USEDEP}] )
 "
 
 PATCHES=(

app-arch/libdeflate/libdeflate-9999.ebuild
@@ -27,7 +27,7 @@ RESTRICT="
 "
 
 DEPEND="
-	test? ( virtual/zlib[${MULTILIB_USEDEP}] )
+	test? ( virtual/zlib:=[${MULTILIB_USEDEP}] )
 "
 
 PATCHES=(

commit 86702ba390771ffab69e470eadaa80441d8dae15

app-arch/libdeflate: remove := from virtual/zlib in test DEPEND
Fixes: d4a25b403dab1c6a6cf69492d27a433519a5619e
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

app-arch/libdeflate/libdeflate-1.23.ebuild
@@ -27,7 +27,7 @@ RESTRICT="
 "
 
 DEPEND="
-	test? ( virtual/zlib:=[${MULTILIB_USEDEP}] )
+	test? ( virtual/zlib[${MULTILIB_USEDEP}] )
 "
 
 PATCHES=(

app-arch/libdeflate/libdeflate-1.24.ebuild
@@ -31,7 +31,7 @@ RESTRICT="
 "
 
 DEPEND="
-	test? ( virtual/zlib:=[${MULTILIB_USEDEP}] )
+	test? ( virtual/zlib[${MULTILIB_USEDEP}] )
 "
 
 PATCHES=(

app-arch/libdeflate/libdeflate-9999.ebuild
@@ -27,7 +27,7 @@ RESTRICT="
 "
 
 DEPEND="
-	test? ( virtual/zlib:=[${MULTILIB_USEDEP}] )
+	test? ( virtual/zlib[${MULTILIB_USEDEP}] )
 "
 
 PATCHES=(