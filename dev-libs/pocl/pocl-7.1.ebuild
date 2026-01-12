negril/gentoo

commit 2555cf78521ee2d92984c09361f2c68d50cd7ae1

dev-libs/pocl: disable LLVM 21
It's broken for nvptx.

```
-- Found CUDAToolkit: /opt/cuda/targets/x86_64-linux/include (found version "12.9.86")
CMake Error at CMakeLists.txt:2202 (message):
  CUDA driver does not work with LLVM/Clang 21

-- Configuring incomplete, errors occurred!
 * ERROR: dev-libs/pocl-7.1::gentoo failed (configure phase):
 *   cmake failed
```

Bug: https://github.com/llvm/llvm-project/issues/154772
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/pocl/pocl-7.1.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
-LLVM_COMPAT=( {18..21} )
+LLVM_COMPAT=( {18..20} )
 inherit cmake cuda llvm-r1
 
 DESCRIPTION="Portable Computing Language (an implementation of OpenCL)"

commit 13bd37c2084f9ce7b02d41f64b989b44e75ea64b

dev-libs/pocl: re-enable LLVM 21 for USE=-cuda
When we don't compile the CUDA driver we can allow LLVM-21.

Fixes: 2555cf78521ee2d92984c09361f2c68d50cd7ae1
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

dev-libs/pocl/pocl-7.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-LLVM_COMPAT=( {18..20} )
+LLVM_COMPAT=( {18..21} )
 inherit cmake cuda llvm-r1
 
 DESCRIPTION="Portable Computing Language (an implementation of OpenCL)"
@@ -15,6 +15,9 @@ SLOT="0"
 KEYWORDS="~amd64 ~ppc64"
 # TODO: hsa tce
 IUSE="accel +conformance cuda debug examples +hwloc memmanager server spirv test"
+# CMake Error at CMakeLists.txt:2202 (message):
+#   CUDA driver does not work with LLVM/Clang 21
+REQUIRED_USE="cuda? ( ^^ ( llvm_slot_18 llvm_slot_19 llvm_slot_20 ) !llvm_slot_21 )"
 RESTRICT="!test? ( test )"
 
 CLANG_DEPS="